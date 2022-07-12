72c6a8e2022ec28f55ffd7a16ced2cde3e788c44
Anahyd-ak <87323728+Anahyd-ak@users.noreply.github.com>
2022-07-12 13:36:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+
+//Crea la variable de canvas usando las propiedades de fabric.
+
+//Coordenadas del personaje
+player_x = 10;
+player_y = 10;
+
+//Tamaño predeterminado de los bloques
+block_image_width = 30;
+block_image_height = 30;
+
+//Crea una variable para almacenar el objeto de la imagen del jugador
+
+function player_update()
+{
+	//Añade el código para que aparezca la imagen del jugador.
+
+	
+}
+
+function new_image(get_image)
+{
+	//Añade el código para que aparezca la imagen de los bloques de construcción.
+	
+
+}
+
+//Clase 87, agregar los controles del controlador de los bloques -SPOILERS-
